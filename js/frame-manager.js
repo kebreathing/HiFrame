ef3a7f811f6a72eccc9e089f0fdda5fbc875f2cd
cn_chris <463720769@qq.com>
2018-05-31 13:16:21 +0800

    迁移文件目录
    1. 已经完成 新建、删除、固定、上下移动
    TODO：
    1. 自定义控件
    2. 截屏
    3. 通知

---
+++ js/frame-manager.js
@@ -0,0 +1,197 @@
+/**
+* Author: chris
+* Created at 2018.5.30
+*/
+
+function _uuid () {
+  return Math.floor(Math.random() * 1000000) + ''
+}
+
+class FrameManager {
+  constructor (pid) {
+    this.pid = pid
+    this.pel = $('#' + pid)
+
+    // 状态变量记录
+    this.isInitial = false
+    this.isMouseDown = false
+
+    // 当前框的相关变量记录
+    this.current_id = undefined
+    this.current_frame = undefined
+
+    // 所有框记录
+    this.frameMap = new Map()
+  }
+
+  // 信息同步：非manager内容
+  syncControlView () {
+    if (this.current_frame === undefined) {
+      $('#btn-fix').text('位置锁定')
+      $('#btn-lock').text('大小锁定')
+      return
+    }
+    $('#btn-fix').text(this.current_frame.isFixed ? '位置解锁' : '位置锁定')
+    $('#btn-lock').text(this.current_frame.isResizable ? '大小解锁' : '大小锁定')
+  }
+
+  validateAction () {
+    return this.current_frame !== undefined
+  }
+}
+
+FrameManager.prototype.initEventListener = function () {
+  function onmousedown (e) {
+    that.isMouseDown = true
+
+    if (e.target) {
+      let id = e.target.id
+      that.current_id = id.split('_')[1]
+      if (id.indexOf('f_') >= 0) {
+        that.isFrame = true
+        // 让上一个current_frame失去焦点
+        that.current_frame !== undefined && that.current_frame.blur()
+        that.current_frame = that.frameMap.get(that.current_id)
+        that.current_frame.focus()
+
+        that.current_frame.record_move()
+        that.syncControlView()
+      } else if (id.indexOf('dot') >= 0) {
+        that.isDot = true
+        that.current_frame !== undefined && that.current_frame.blur()
+        that.current_frame = that.frameMap.get(that.current_id)
+        that.current_frame.focus()
+
+        that.current_frame.record_move()
+        that.current_frame.record_dot(id)
+        that.syncControlView()
+      }
+
+      that.diffX = e.clientX
+      that.diffY = e.clientY
+    }
+  }
+
+  function onmousemove (e) {
+    if (!that.isMouseDown) {
+      return
+    }
+
+
+    let dx = e.clientX - that.diffX
+    let dy = e.clientY - that.diffY
+
+    // 获取当前截点id
+    if (that.isFrame) {
+      that.current_frame.setPos(dy, dx)
+    } else if (that.isDot) {
+      that.current_frame.setSize(dy, dx)
+    }
+  }
+
+  function onmouseup (e) {
+    that.isMouseDown = that.isFrame = that.isDot = false
+    that.diffX = that.diffY = 0
+  }
+
+  // 事件绑定
+  var that = this
+
+  $(document).bind('mousedown', onmousedown)
+  $(document).bind('mousemove', onmousemove)
+  $(document).bind('mouseup', onmouseup)
+}
+
+FrameManager.prototype.addFrame = function () {
+  let fid = _uuid()
+  let frame = new Frame(fid, this.pid)
+
+  this.pel.append(frame.get_el())
+  this.current_frame !== undefined && this.current_frame.blur()
+  this.current_frame = frame
+  this.current_frame.focus()
+
+  this.frameMap.set(fid, frame)
+  this.syncControlView()
+}
+
+FrameManager.prototype.deleteFrame = function () {
+  if (!this.validateAction()) {
+    return
+  }
+
+  this.frameMap.delete(this.current_id)
+  this.current_frame.delete()
+
+  this.current_id = undefined
+  this.current_frame = undefined
+  this.syncControlView()
+}
+
+FrameManager.prototype.fixFrame = function () {
+  // 固定当前框位置
+  if (!this.validateAction()) {
+    return
+  }
+
+  let isFixed = !this.current_frame.isFixed
+  this.current_frame.setFixed(isFixed)
+  return isFixed
+}
+
+
+FrameManager.prototype.lockFrame = function () {
+  // 固定当前框大小
+  if (!this.validateAction()) {
+    return
+  }
+
+  let isLocked = !this.current_frame.isResizable
+  this.current_frame.setResizable(isLocked)
+  return isLocked
+}
+
+FrameManager.prototype.modifyFrameLayer = function (offset) {
+  // 修改框的层级
+  if (!this.validateAction()) {
+    return
+  }
+
+  this.current_frame.setZIndex(offset)
+}
+
+
+// 事件处理程序
+function onCreateFrame (e) {
+  manager.addFrame()
+}
+
+// 删除当前框
+function onDeleteFrame (e) {
+  manager.deleteFrame()
+}
+
+// 位置锁定
+function onFixFrame (e) {
+  e.innerHTML = manager.fixFrame() ? '位置解锁' : '位置锁定'
+}
+
+// 大小锁定
+function onLockFrame (e) {
+  e.innerHTML = manager.lockFrame() ? '大小解锁' : '大小锁定'
+}
+
+// 上移层
+function onUpperFrame (e) {
+  manager.modifyFrameLayer(1)
+}
+
+// 下移层
+function onLowerFrame (e) {
+  manager.modifyFrameLayer(-1)
+}
+
+$(document).ready(function () {
+  // var manager = new FrameManager("parent")
+  // manager.initEventListener()
+})
--- js/frame.js
+++ js/frame.js
@@ -1,225 +1,201 @@
-// js of Draggable frame
-class DraggableFrame {
-  /**
-  * id: attribute of element
-  * css: editable css
-  */
-  constructor (id, parentId) {
-    this.id = id
-    this.parentId = parentId
-  }
-  init () {
-    this.parentEl = $('#' + this.parentId)
-    // create element
-    this.el = $('<div class="draggable-frame" id="' + this.id + '"></div>')
-    this.dot1 = $('<div class="dot" id="dot1"></div>')
-    this.dot2 = $('<div class="dot" id="dot2"></div>')
-    this.dot3 = $('<div class="dot" id="dot3"></div>')
-    this.dot4 = $('<div class="dot" id="dot4"></div>')
-    this.focus_dot = $('<div class="focus-dot"></div>')
-
-    this.el.css('visibility', 'visible')
-    this.el.append(this.dot1)
-            .append(this.dot2)
-            .append(this.dot3)
-            .append(this.dot4)
-            .append(this.focus_dot)
-
-    this.initFrameEventListener()
-    this.initDotEventListener()
-
-    this.parentEl.append(this.el)
-  }
-  /**
-  * 框事件绑定
-  */
-  initFrameEventListener () {
-    var that = this
-
-    // Movable Frame
-    function onFrameMovable (e) {
-      e.stopPropagation()
-      // 获得焦点
-      that.get_focus()
-      var diffX = e.clientX
-      var diffY = e.clientY
-
-      // constant
-      var _top = that.int_css_of_el(that.el, 'top')
-      var _left = that.int_css_of_el(that.el, 'left')
-      var _width = that.int_css_of_el(that.el, 'width')
-      var _height = that.int_css_of_el(that.el, 'height')
-      var _pwidth = that.int_css_of_el(that.parentEl, 'width')
-      var _pheight = that.int_css_of_el(that.parentEl, 'height')
-
-      function mousemove (e2) {
-        let dx = e2.clientX - diffX
-        let dy = e2.clientY - diffY
-        let nx = _left + dx
-        let ny = _top + dy
-
-        if (nx < 0 || ny < 0 || nx + _width > _pwidth || ny + _height > _pheight) {
-          // do nothing
-        } else {
-          that.el.css({'top': ny  + 'px', 'left': nx  + 'px'})
-        }
-      }
-
-      function mouseup (e2) {
-        $(document).unbind('mousemove', mousemove)
-        $(document).unbind('mouseup', mouseup)
-      }
-
-      $(document).bind('mousemove', mousemove)
-      $(document).bind('mouseup', mouseup)
-    }
-
-    this.el.bind('mousedown', onFrameMovable)
-  }
-  initDotEventListener () {
-    var that = this
-    // 点击事件绑定在dot上
-    function dotMousedown (e) {
-      e.stopPropagation()
-      // 获得焦点
-      that.get_focus()
-
-      var dotId = e.currentTarget.id
-
-      // frame对的宽高
-      var _width = that.int_css_of_el(that.el, 'width')
-      var _height = that.int_css_of_el(that.el, 'height')
-      var _pwidth = that.int_css_of_el(that.parentEl, 'width')
-      var _pheight = that.int_css_of_el(that.parentEl, 'height')
-
-      // position
-      var _top = that.int_css_of_el(that.el, 'top')
-      var _left= that.int_css_of_el(that.el, 'left')
-      var diffX = e.clientX
-      var diffY = e.clientY
-
-      function dmousemove (e2) {
-        let dx = e2.clientX - diffX
-        let dy = e2.clientY - diffY
-
-        // new width
-        switch (dotId) {
-          case 'dot1':
-            dx = 0 - dx
-            dy = 0 - dy
-            if (_top - dy < 0
-              || _left - dx < 0) {
-              // do nothiing
-            } else {
-              that.el.css({
-                'top': _top - dy + 'px',
-                'left': _left - dx + 'px',
-                'width': _width + dx + 'px',
-                'height': _height + dy + 'px'
-              })
-            }
-            break;
-          case 'dot2':
-            dy = 0 - dy
-            if (_top - dy < 0
-              || _left + _width + dx > _pwidth) {
-              // do nothing
-            } else {
-              that.el.css({
-                'top': _top - dy + 'px',
-                'width': _width + dx + 'px',
-                'height': _height + dy + 'px'
-              })
-            }
-            break;
-          case 'dot3':
-            dx = 0 - dx
-            if (_left - dx < 0
-              || _top + _height + dy > _pheight) {
-              // do nothing
-            } else {
-              that.el.css({
-                'left': _left - dx + 'px',
-                'width': _width + dx + 'px',
-                'height': _height + dy + 'px'
-              })
-            }
-            break;
-          case 'dot4':
-            if (_left + _width + dx > _pwidth
-              || _top + _height + dy > _pheight) {
-              // do nothing
-            } else {
-              that.el.css({
-                'width': _width + dx + 'px',
-                'height': _height + dy + 'px'
-              })
-            }
-            break;
-          default:
-            console.log('no more dots.')
-        }
-      }
-
-      function dmouseup (e2) {
-        $(document).unbind('mousemove', dmousemove)
-        $(document).unbind('mouseup', dmouseup)
-      }
-
-      $(document).bind('mousemove', dmousemove)
-      $(document).bind('mouseup', dmouseup)
-    }
-    // 移动事件绑定在parent中
-
-    this.dot1.bind('mousedown', dotMousedown)
-    this.dot2.bind('mousedown', dotMousedown)
-    this.dot3.bind('mousedown', dotMousedown)
-    this.dot4.bind('mousedown', dotMousedown)
-  }
-  setCSS (css) {
-    if (css === undefined) {
-      throw new Exception('CSS cannot be null')
-    }
-
-    this.el.css(css)
-  }
-  int_css_of_el (el, cssName) {
-    return parseInt(el.css(cssName))
-  }
-  lost_focus () {
-    this.el.children('.focus-dot').css({'visibility': 'hidden'})
-  }
-  get_focus () {
-    this.el.children('.focus-dot').css({'visibility': 'visible'})
-  }
-  get_el () {
-    return this.el
+/**
+* Author: chris
+* Created at 2018.5.30
+*/
+const _FRAME_STR = {
+  container: function (id) {
+    return '<div class="frame c-border-blue" id="' + id + '"></div>'
+  },
+  dotlu: function (id) {
+    return '<i class="fa fa-circle dot dot-lu c-dot-blue" id="' + id + '"></i>'
+  },
+  dotru: function (id) {
+    return '<i class="fa fa-circle dot dot-ru c-dot-blue" id="' + id + '"></i>'
+  },
+  dotlb: function (id) {
+    return '<i class="fa fa-circle dot dot-lb c-dot-blue" id="' + id + '"></i>'
+  },
+  dotrb: function (id) {
+    return '<i class="fa fa-circle dot dot-rb c-dot-blue" id="' + id + '"></i>'
+  },
+  layer: function (id, text) {
+    return '<span class="layer c-dot-blue">' + text + '</span>'
   }
 }
 
-// 建立DraggableFrame的管理池 manager
-class FrameManager {
-  constructor () {
-    this.frames = []
-    this.current_frame = undefined
+const _FRAME_STYLE = {
+  focus_frame: 'c-border-yellow',
+  focus_dot: 'c-dot-yellow',
+  active_dot: 'c-dot-red',
+  blur_frame: 'c-border-blue',
+  blur_dot: 'c-dot-blue'
+}
+
+function _get_dot (id) {
+  if (id.indexOf('lu') >= 0) {
+    return 0
+  } else if (id.indexOf('ru') >= 0) {
+    return 1
+  } else if (id.indexOf('lb') >= 0) {
+    return 2
+  } else if (id.indexOf('rb') >= 0) {
+    return 3
   }
-  createFrame (id, parentId, css) {
+}
+
+function _int (str) {
+  return parseInt(str)
+}
+
+class Frame {
+  constructor (id, pid) {
+    this.id = id
+    this.pid = pid
+
+    // 是否固定
+    this.isResizable = false
+    this.isFixed = false
+    this.create()
+  }
+
+  create () {
     var that = this
-    if (this.current_frame !== undefined) {
-        this.current_frame.lost_focus()
+
+    // 可自定义选择组件
+    var el = $(_FRAME_STR.container('f_' + this.id))
+    var dlu = $(_FRAME_STR.dotlu('dotlu_' + this.id))
+    var dru = $(_FRAME_STR.dotru('dotru_' + this.id))
+    var dlb = $(_FRAME_STR.dotlb('dotlb_' + this.id))
+    var drb = $(_FRAME_STR.dotrb('dotrb_' + this.id))
+    var slayer = $(_FRAME_STR.layer('span_' + this.id, 5))
+    el.append(dlu)
+        .append(dru)
+          .append(dlb)
+            .append(drb)
+              .append(slayer)
+
+    this.el = el
+    this.dlu = dlu
+    this.dru = dru
+    this.dlb = dlu
+    this.drb = drb
+    this.slayer = slayer
+  }
+
+  // 记录frame的当前坐标
+  record_move () {
+    this.startX = _int(this.el.css('left'))
+    this.startY = _int(this.el.css('top'))
+  }
+
+  // 记录当前被拖动的点
+  record_dot (id) {
+    this.dotType = _get_dot(id)
+    this.startWidth = _int(this.el.css('width'))
+    this.startHeight = _int(this.el.css('height'))
+  }
+
+  get_el () {
+    return this.el
+  }
+
+  setResizable (value) {
+    this.isResizable = value
+  }
+
+  setFixed (value) {
+    this.isFixed = value
+  }
+
+  setZIndex (value) {
+    let zindex = _int(this.el.css('z-index')) + value
+
+    if (value < 0) {
+      return
     }
 
-    var instance = new DraggableFrame(id, parentId, css)
-    instance.init()
-    instance.setCSS(css)
-    instance.get_focus()
-    instance.get_el().bind('click', function () {
-      that.current_frame.lost_focus()
-      instance.get_focus()
-      that.current_frame = instance
-    })
-
-    this.current_frame = instance
-    this.frames.push({instance: instance, visibility: true, focus: false})
+    this.slayer.text(zindex)
+    this.el.css({'z-index': zindex})
   }
+
+  setPos (top, left) {
+    if (this.isFixed) {
+      console.log('已经固定: isFixed = true')
+      return
+    }
+
+    // 框坐标设置
+    let _top = this.startY + top + 'px'
+    let _left = this.startX + left + 'px'
+
+    this.el.css({
+      'top': _top,
+      'left': _left
+    })
+  }
+
+  setSize (top, left) {
+    if (this.isResizable) {
+      console.log('不可改变: isResizable = true')
+      return
+    }
+    var that = this
+
+    var _width = that.startWidth
+    var _height = that.startHeight
+    var _x = that.startX
+    var _y = that.startY
+
+    // 固定位置
+    switch (that.dotType) {
+      case 0:
+        top = -top
+        left = -left
+        that.el.css({
+          'top': _y - top + 'px',
+          'left': _x - left + 'px'
+        })
+        break;
+      case 1:
+        top = -top
+        that.el.css({
+          'top': _y - top + 'px'
+        })
+        break;
+      case 2:
+        left = -left
+        that.el.css({
+          'left': _x - left + 'px'
+        })
+        break
+      case 3:
+        break;
+      default:
+        console.log('add your own position function')
+    }
+
+    that.el.css({
+      'width': _width + left + 'px',
+      'height': _height + top + 'px'
+    })
+  }
+}
+
+// 失去焦点
+Frame.prototype.blur = function (cb) {
+  $('#f_' + this.id).removeClass(_FRAME_STYLE.focus_frame)
+  $('#f_' + this.id).children().removeClass(_FRAME_STYLE.focus_dot)
+  return true
+}
+
+// 获取焦点
+Frame.prototype.focus = function (cb) {
+  $('#f_' + this.id).addClass(_FRAME_STYLE.focus_frame)
+  $('#f_' + this.id).children().addClass(_FRAME_STYLE.focus_dot)
+  return true
+}
+
+// 删除
+Frame.prototype.delete = function (cb) {
+  $('#f_' + this.id).remove()
 }
